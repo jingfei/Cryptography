02562917412b0c2b2af6603bdc65059b184d9965
jingfei <jingfei955047@gmail.com>
2016-08-02 00:12:55 +0800

    Add frequency chart.

--- Stage1.js
+++ Stage1.js
@@ -1,3 +1,4 @@
+/* table translate */
 var alph = [];
 for(var i = 0; i<26; ++i) alph[i] = false;
 
@@ -26,26 +27,52 @@ String.prototype.replaceAt = function(index, character){
 }
 
 function Convert(){
-	var $Text = $('#Input').val();
-	var $Ans = $Text;
-	if(!$Text) return;
+	var input = $('#Input').val();
+	var $Ans = input;
+	if(!input) return;
 	/* change all input to lower-alpha */
-	$Text = $Text.toLowerCase();
-	$('#Input').val($Text);
+	input = input.toLowerCase();
+	$('#Input').val(input);
 	/***********************************/
+
+	/* show frequency chart */
+	var data = [];
+	var total = input.length;
+	var i = 97;
+	while(true){
+		var ch = String.fromCharCode(i);
+		var tmp_ch = ch;
+		if(ch=="(" || ch==")" || ch=="[" || ch=="{" || ch=="*" || ch=="+" || ch=="." || ch=="$" || ch=="^" || ch=="\\" || ch=="|" || ch=="?")
+			tmp_ch = "\\" + ch;
+		var count = (input.match(new RegExp(tmp_ch,"g")) || []).length; 
+		if(count !== 0) {
+			if(i>=97 && i<=122) ch=String.fromCharCode(i-(97-65));
+			data.push({alph:ch, frequency: count/total});
+		}
+
+		/* change the sequence of alph in chart */
+		i++;
+		if(i===122) i=48;
+		else if(i===58) i=33;
+		else if(i===48) i=58;
+		else if(i===65) i=91;
+		else if(i===97) i=122;
+		else if(i===127) break;
+	}
+	showChart(data);
+
+	/* convert to result */
 	for($i=33; $i<127; $i++){
 		if($i>=65 && $i<=90) continue;
 		var $Before = String.fromCharCode($i);
-		var $After = $('#a'+$i).html();
-		if($After.length===0) $After = $('#a'+$i).val();
-		if($After.length===0) $After = $Before;
+		var $After = $('#a'+$i).html() || $('#a'+$i).val() || $Before;
 		if($Before=="(" || $Before==")" || $Before=="[" || $Before=="{" || $Before=="*" || $Before=="+" || $Before=="." || $Before=="$" || $Before=="^" || $Before=="\\" || $Before=="|" || $Before=="?")
 			$Before = "\\" + $Before;
-		$n = $Text.search($Before);
+		$n = input.search($Before);
 		while($n!=-1){
 			$Ans = $Ans.replaceAt($n,$After);
-			$Text = $Text.replaceAt($n," ");
-			$n = $Text.search($Before);
+			input = input.replaceAt($n," ");
+			$n = input.search($Before);
 		}
 	}
 	$('#Output').val($Ans);
@@ -103,5 +130,66 @@ function buildOtherTable(){
 	for(var i=21; i<len; ++i) inner += "<td><input type='text' class='form-control' placeholder='"+ String.fromCharCode(ar[i]) +"' maxlength='1' id='a"+ ar[i] +"' /></td>";
 	inner += "</tr>";
 	$("#table-other2").html(inner);
+}
+
+/* frequency */
+function showChart(data){
+	$("#barChart").html("");
+	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+		width = $("#Main").width()*.9 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+	
+	var x = d3.scale.ordinal()
+		.rangeRoundBands([0, width], .1);
+	
+	var y = d3.scale.linear()
+						.range([height, 0]);
+	
+	var xAxis = d3.svg.axis()
+								.scale(x)
+								.orient("bottom");
+	
+	var yAxis = d3.svg.axis()
+								.scale(y)
+								.orient("left")
+								.ticks(10, "%");
+	
+	var svg = d3.select("#barChart").append("svg")
+							.attr("width", width + margin.left + margin.right)
+							.attr("height", height + margin.top + margin.bottom)
+							.append("g")
+							.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+	x.domain(data.map(function(d) { return d.alph; }));
+	y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+	
+	svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis);
+	
+	svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis)
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text("Frequency");
+	
+	svg.selectAll(".bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return x(d.alph); })
+			.attr("width", x.rangeBand())
+			.attr("y", function(d) { return y(d.frequency); })
+			.attr("height", function(d) { return height - y(d.frequency); });
+}
+
+function type(d) {
+	d.frequency = +d.frequency;
+	return d;
 }
 
